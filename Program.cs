40eaa821c6da6ffb34bc5c92e3bf2397889ca587
Radu Elisei <45122923+raduElisei@users.noreply.github.com>
2023-10-03 19:13:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab2Problema1EliseiRadu;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Scrie numele tau: ");
+        var name = Console.ReadLine();
+        Console.WriteLine("Scrie varsta ta: ");
+        try
+        {
+            int varsta = int.Parse(Console.ReadLine());
+            if (varsta < 18)
+            {
+                Console.WriteLine(name + ", nu ai varsta necesara pentru a vota.");
+            }
+            else
+            {
+                Console.WriteLine("Poti vota, " + name + ".");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Inputul nu este un numar natural.");
+        }
+
+    }
+}
